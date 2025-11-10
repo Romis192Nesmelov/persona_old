b77f7715a07d991381c49272ef7f6afebfa60052
Romis <romis@nesmelov.com>
2025-11-10 16:52:28 +0300

    changed the base_url

--- application/config/config.php
+++ application/config/config.php
@@ -29,7 +29,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = (ENVIRONMENT == 'production') ? 'https://persona-city.ru/' : 'http://127.0.0.1:8098/';
+$config['base_url'] = '/';
 
 /*
 |--------------------------------------------------------------------------
