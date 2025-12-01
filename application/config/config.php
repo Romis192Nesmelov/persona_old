7f891ba1560e12232cab17b25ce1a36640c1d5cf
Romis <romis@nesmelov.com>
2025-12-01 12:53:36 +0300

    changed config

--- application/config/config.php
+++ application/config/config.php
@@ -29,7 +29,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '/';
+$config['base_url'] = (ENVIRONMENT == 'development') ? 'https://localhost/Persona-City.Web/' : '/';
 
 /*
 |--------------------------------------------------------------------------
